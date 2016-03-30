220687a0eac5778769b5f9ee492d5679100578e5
Akash A <akash1810@gmail.com>
2016-03-30 20:38:18 +0100

    adding crude token revoke flow

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const express = require('express');
 const google = require('googleapis');
 const OAuth2 = google.auth.OAuth2;
 const url = require('url');
+const request = require('request');
 const settings = require('./settings');
 
 const runApp = (settings, oauth2Client) => {
     const host = url.parse(settings.host);
-    const port = host.port || 9000;
+    const port = host.port || 8080;
     const callbackPath = url.parse(settings.callback).pathname;
 
     const app = express();
@@ -17,8 +18,32 @@ const runApp = (settings, oauth2Client) => {
         const code = req.query.code;
 
         oauth2Client.getToken(code, (err, token) => {
+            if (token.refresh_token) {
+                token.revoke_url = url.resolve(host.href, `/revoke?token=${token.refresh_token}`);
+            }
+
             res.json(token);
         });
+    });
+
+    app.get('/revoke', (req, res) => {
+        const token = req.query.token;
+
+        if (token) {
+            request(`https://accounts.google.com/o/oauth2/revoke?token=${token}`, (error, response, body) => {
+                if (error) {
+                    console.error(error);
+                    res.status(500).send(error).end();
+                }else {
+                    response.statusCode === 200 ?
+                        res.json({auth: url.resolve(host.href, '/')}) :
+                        res.status(response.statusCode).json(JSON.parse(body));
+                }
+            });
+        }
+        else {
+            res.redirect('/');
+        }
     });
 
     app.get('/', (req, res) => {
@@ -28,7 +53,7 @@ const runApp = (settings, oauth2Client) => {
         }));
     });
 
-    app.listen(port, () => console.log('Server listening on ' + port));
+    app.listen(port, () => console.log(`Server listening on ${port}`));
 
     return app;
 };
